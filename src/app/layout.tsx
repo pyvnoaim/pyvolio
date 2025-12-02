9a08707749df359b2850bf9d16f429c036e0ae1d
pyvnoaim <pyvnovlr@gmail.com>
2025-12-02 19:51:52 +0100

    updated metadata and added pictures

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,38 @@ const jetbrains = JetBrains_Mono({
 });
 
 export const metadata: Metadata = {
-  title: '@pyvnoaim',
+  metadataBase: new URL('https://pyvno.xyz'),
+  title: 'pyvolio',
   description: '+aim +peripherals',
+  keywords: ['gaming', 'peripherals', 'aim', 'tech', 'pyvolio', 'pyvno'],
+  authors: [{ name: 'pyvno' }],
+  openGraph: {
+    title: 'pyvolio',
+    description: '+aim +peripherals',
+    url: 'https://pyvno.xyz/',
+    siteName: 'pyvolio',
+    images: [
+      {
+        url: '/banner.png',
+        width: 1200,
+        height: 630,
+        alt: 'pyvolio',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'pyvolio',
+    description: '+aim +peripherals',
+    images: ['/banner.png'],
+  },
+};
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  themeColor: '#ff9a9a',
 };
 
 export default function RootLayout({

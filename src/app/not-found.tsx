3581e725be7c663fb96e3ab738aaf2d4723f420f
pyvnoaim <pyvnovlr@gmail.com>
2025-12-08 22:13:37 +0100

    updated not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,16 @@
+import TextType from '@/components/TextType'
+
 export default function NotFound() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-4 p-6 text-center">
-      <h2 className="text-3xl font-semibold">Page Not Found</h2>
-      <p className="">The resource you’re looking for doesn’t exist</p>
+      <TextType
+        text={['page not found', '404 error']}
+        cursorCharacter="_"
+        typingSpeed={75}
+        deletingSpeed={75}
+        className="text-3xl font-semibold"
+      />
+      <p className="">the resource you’re looking for doesn’t exist</p>
     </div>
   )
 }

a7afe522136d7da912d9a55f3d4b9725e1e2d183
pyvnoaim <pyvnovlr@gmail.com>
2025-12-27 01:57:29 +0100

    fixed kovaaks metadata

---
+++ src/app/kovaaks/layout.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from 'next'
+import { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: 'kovaaks',
+}
+
+export default function PeripheralsLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center gap-6 p-4 sm:p-6">
+      {children}
+    </div>
+  )
+}
